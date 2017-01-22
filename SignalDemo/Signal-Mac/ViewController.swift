fe9fde4f7b2d3b38a1319548653fb1b2783a19e4
kirankunigiri <kirankunigiri@gmail.com>
2017-01-21 22:59:23 -0800

    Created Xcode project

---
+++ SignalDemo/Signal-Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Signal-Mac
+//
+//  Created by Kiran Kunigiri on 1/21/17.
+//  Copyright © 2017 Kiran. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ SignalDemo/Signal-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Signal-iOS
+//
+//  Created by Kiran Kunigiri on 1/21/17.
+//  Copyright © 2017 Kiran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
